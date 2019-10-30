1acf79a31ce7e5f0e00d5a0301cf9a88081e30c8
XiaoSong9905 <songxiao527@gmail.com>
2019-10-30 13:13:24 -0400

    update cfg

--- main.py
+++ main.py
@@ -29,23 +29,27 @@ parser.add_argument("-style_image", help="./path/file to style image", default='
 parser.add_argument("-native_image", help="./path/file to simple stich image", default='data/0_naive.jpg')
 parser.add_argument("-tight_mask", help="./path/file to tight mask", default='data/0_c_mask.jpg')
 parser.add_argument("-dilated_mask", help="./path/file to dilated(loss) mask", default='data/0_c_mask_dilated.jpg')
-parser.add_argument("-output_img", help="./path/file for output image", default='output/0_output.jpg')
+parser.add_argument("-p1_output_img", help="./path/file for output image", default='output/0_pass1_out.jpg')
+parser.add_argument("-p2_output_img", help="./path/file for output image", default='output/0_pass2_out.jpg')
 parser.add_argument("-output_img_size", help="Max side(H/W) for output image", type=int, default=500)
 
 # Training Parameter 
 parser.add_argument("-optim", choices=['lbfgs', 'adam'], default='lbfgs')
 parser.add_argument("-lr", type=float, default=1e-2)
-parser.add_argument("-n_iters", type=int, default=1000)
+parser.add_argument("-p1_n_iters", type=int, default=1000)
+parser.add_argument("-p2_n_iters", type=int, default=1000)
 parser.add_argument("-print_interval", type=int, default=100)
 parser.add_argument("-save_img_interval", type=int, default=100)
 parser.add_argument("-log_interval", type=int, default=100)
 parser.add_argument("-gpu", help="Zero-indexed ID of the GPU to use; for CPU mode set -gpu = -1", default=-1)
 
 # Model Parameter 
-parser.add_argument("-content_layers", help="layers for content", default='relu4_2')
-parser.add_argument("-style_layers", help="layers for style", default='relu1_1,relu2_1,relu3_1,relu4_1,relu5_1')
+parser.add_argument("-p1_content_layers", help="layers for content", default='relu4_2')
+parser.add_argument("-p1_style_layers", help="layers for style", default='relu1_1,relu2_1,relu3_1,relu4_1,relu5_1')
+parser.add_argument("-p2_content_layers", help="single layer for content", default='relu4_2')
+parser.add_argument("-p2_style_layers", help="single layer for style", default='relu4_2')
 parser.add_argument("-content_weight", type=float, default=5)
-parser.add_argument("-style_weight", type=float, default=100)
+parser.add_argument("-style_weight", type=float, default=100) # can setup content loss, style loss seprately for pass1 pass2
 parser.add_argument("-tv_weight", type=float, default=1e-3)
 parser.add_argument("-init", choices=['random', 'image'], default='image')
 parser.add_argument("-model_file", help="path/file to saved model file, if not will auto download", default=None)
@@ -70,8 +74,8 @@ def pass1():
     loss_mask = mask_preprocess(cfg.dilated_mask, cfg.output_img_size, dtype, device, cfg,name='read_loss_mask.png')
 
     # Setup Network 
-    content_layers = cfg.content_layers.split(',')
-    style_layers = cfg.style_layers.split(',')
+    content_layers = cfg.p1_content_layers.split(',')
+    style_layers = cfg.p1_style_layers.split(',')
     content_loss_list = []
     style_loss_list = []
     tv_loss = []
@@ -94,26 +98,18 @@ def pass1():
             net.add_module(str(len(net)), layer)
             sap = nn.AvgPool2d(kernel_size=3, stride=1, padding=1)
             #print('mask before sap', mask)
-            #torchvision.utils.save_image(mask, '{}.png'.format('sap_mask_'+str(i)+'_before' ))
             #save_img_plt(mask, 'sap_mask_'+str(i)+'_before.png', gray=True)
             mask = sap(mask)
             #save_img_plt(mask, 'sap_mask_'+str(i)+'_after.png', gray=True)
-            #torchvision.utils.save_image(mask, '{}.png'.format('sap_mask_'+str(i)+'_after' ))
             #print('mask after sap', mask)
         elif isinstance(layer, nn.ReLU):
             net.add_module(str(len(net)), layer)
         elif isinstance(layer, nn.MaxPool2d) or isinstance(layer, nn.AvgPool2d):
             net.add_module(str(len(net)), layer)
-            #resize = transforms.Resize((math.floor(mask.shape[1]/2), math.floor(mask.shape[0]/2)))
-            #torchvision.utils.save_image(mask, '{}.png'.format('resize_mask_'+str(i)+'_before'))
             #save_img_plt(mask, 'resize_mask_'+str(i)+'_before.png', gray=True)
-            #print('mask dim', mask.dim())
             #print(mask.shape)
             mask = F.interpolate(mask, scale_factor=(0.5, 0.5))
-            #mask = F.interpolate(mask, size=(math.floor(mask.shape[1]/2), math.floor(mask.shape[0]/2)))
             #save_img_plt(mask, 'resize_mask_'+str(i)+'_after.png', gray=True)
-            #mask = resize(mask)
-            #torchvision.utils.save_image(mask, '{}.png'.format('resize_mask_'+str(i)+'_after'))
 
         # Add Loss layer 
         if layer_list[i] in content_layers:
@@ -193,7 +189,7 @@ def pass1():
     
     optimizer = build_optimizer(cfg, native_img)
     i_iter = 0
-    while i_iter <= cfg.n_iters:
+    while i_iter <= cfg.p1_n_iters:
         optimizer.step(closure)
     
     print('===>deep painterly harmonization pass 1 is done')

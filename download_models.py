2a54cf420ac9a1c9f219633ab66d8f84ffd25abd
XiaoSong9905 <songxiao527@gmail.com>
2019-11-25 23:33:31 -0500

    chaenge

--- download_models.py
+++ download_models.py
@@ -10,8 +10,9 @@ print("Downloading the VGG-19 model")
 sd = load_url("https://s3-us-west-2.amazonaws.com/jcjohns-models/vgg19-d01eb7cb.pth")
 map = {'classifier.1.weight':u'classifier.0.weight', 'classifier.1.bias':u'classifier.0.bias', 'classifier.4.weight':u'classifier.3.weight', 'classifier.4.bias':u'classifier.3.bias'}
 sd = OrderedDict([(map[k] if k in map else k,v) for k,v in sd.items()])
-torch.save(sd, path.join("models", "vgg19-d01eb7cb.pth"))
-
+#torch.save(sd, path.join("models", "vgg19-d01eb7cb.pth"))
+torch.save(sd, 'vgg19-d01eb7cb.pth')
+'''
 # Download the VGG-16 model and fix the layer names
 print("Downloading the VGG-16 model")
 sd = load_url("https://s3-us-west-2.amazonaws.com/jcjohns-models/vgg16-00b39a1b.pth")
@@ -27,5 +28,5 @@ if version_info[0] < 3:
 else: 
     import urllib.request
     urllib.request.urlretrieve("https://raw.githubusercontent.com/ProGamerGov/pytorch-nin/master/nin_imagenet.pth", path.join("models", "nin_imagenet.pth"))
-
+'''
 print("All models have been successfully downloaded")

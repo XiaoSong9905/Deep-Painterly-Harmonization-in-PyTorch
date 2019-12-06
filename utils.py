7ef5f5bbf834566d48f2c31360601b10cb0ed502
XiaoSong9905 <songxiao527@gmail.com>
2019-12-05 21:54:24 -0500

    add support dip

--- utils.py
+++ utils.py
@@ -80,11 +80,13 @@ def get_args():
     parser.add_argument("-style_weight", type=float, default=1e2) 
     parser.add_argument("-tv_weight", type=float, default=1e-3)
     parser.add_argument("-histogram_weight", type=float, default=0) # For pass 2, commonly use '1e2'
-    parser.add_argument('-mse_loss_outside_mask_weight', type=float, default=1)
-    parser.add_argument('-mse_loss_within_mask_weight', type=float, default=1e2)
+    parser.add_argument('-mse_loss_outside_mask_weight', type=float, default=1) # For DIP ANALYSIS 
+    parser.add_argument('-mse_loss_within_mask_weight', type=float, default=1e2) # For DIP ANALYSIS 
     parser.add_argument("-model_file", help="path/file to saved model file, if not will auto download", default='./download_model_weight/vgg19-d01eb7cb.pth')
     parser.add_argument("-model", choices=['vgg16', 'vgg19'], default='vgg19')
     parser.add_argument("-match_patch_size", type=int, default=3)
+    parser.add_argument("-generator_model", choices=['skip_depth6', 'skip_depth4', 'skip_depth2', 'UNET', 'ResNet']) # For DIP ANALYSIS 
+    parser.add_argument("-noise_input_depth", type=int, default=1) # input depth for noise input to generator model 
 
     # Other 
     parser.add_argument("-mask_on", choices=['on', 'off'], default='on') # if 'off' is choose, no mask is use, match is computed between whole content image fm and style image fm

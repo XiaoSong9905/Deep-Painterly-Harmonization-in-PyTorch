b06610f1ad2349038fb66f79ee8468a4d0168a9f
Deyang Dai <deyangd@umich.edu>
2019-12-07 00:52:04 -0500

    fixed a bug

--- model.py
+++ model.py
@@ -567,15 +567,15 @@ class StyleLossPass2(StyleLossPass1):
 
         for i in range(curr_h):
             for j in range(curr_w):
-                ref_idx = [(i + 0.5) // h_ratio, (j + 0.5) // w_ratio]
+                ref_idx = [(i + 0.4999) // h_ratio, (j + 0.4999) // w_ratio]
                 ref_idx[0] = int(max(min(ref_idx[0], ref_h - 1), 0))
                 ref_idx[1] = int(max(min(ref_idx[1], ref_w - 1), 0))
 
                 ref_mapping_idx = ref_corr[ref_idx[0], ref_idx[1]]
                 ref_mapping_idx = (ref_mapping_idx // ref_w, ref_mapping_idx % ref_w)
 
-                curr_mapping_idx = (int(i + (ref_mapping_idx[0] - ref_idx[0]) * h_ratio + 0.5),
-                                    int(j + (ref_mapping_idx[1] - ref_idx[1]) * w_ratio + 0.5))
+                curr_mapping_idx = (int(i + (ref_mapping_idx[0] - ref_idx[0]) * h_ratio + 0.4999),
+                                    int(j + (ref_mapping_idx[1] - ref_idx[1]) * w_ratio + 0.4999))
                 curr_corr[i, j] = curr_mapping_idx[0] * curr_w + curr_mapping_idx[1]
 
                 #  1 * C * 3 * 3

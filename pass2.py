e01860518fdbd6d49aed2a9ea159a13948f8a3f0
XiaoSong9905 <songxiao527@gmail.com>
2019-12-07 16:19:23 -0500

    debug

--- pass2.py
+++ pass2.py
@@ -47,9 +47,9 @@ def capture_fm_pass2(content_loss_list, style_loss_list, tv_loss_list, histogram
     tmp_ref_corr = None
     for idx, i in reversed(list(enumerate(style_loss_list))):  # TODO: change ref layer, and other layers
         if not i.mode == 'capture_style_ref':
-            i.ref_infor = tmp_ref_corr
+            i.ref_corr = tmp_ref_corr
         else:
-            tmp_ref_corr = i.ref_infor
+            tmp_ref_corr = i.ref_corr
             i.mode = 'None'
     net(style_img)
 
